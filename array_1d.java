6bca5b3df9cdf1c0dda7bccaffe45d185972794e
deepthikn49-cloud <deepthikn49@gmail.com>
2026-02-12 12:42:08 +0530

    Completed till string

--- array_1d.java
+++ array_1d.java
@@ -20,6 +20,33 @@ class Array1DEnhanced{
         System.out.println("Sum:" +sum);
     }
 }
+class demo{
+    public static void main(String[] args) {
+        String[] playlist={"song1","song2","song3"};
+         String[] playlist1=new String[3];
+         playlist1[0]="song1";
+         System.out.println(playlist1[0]);
+         playlist1[1]="song2";
+         System.out.println(playlist1[1]);
+          
+    }
+}
+
+class demo2{
+    public static void main(String[] args) {
+        int[] numbers={10,30,60,80};
+        int target=30;
+        for(int i=0;i<numbers.length;i++){
+            if(numbers[i]==target){
+                target=numbers[i];
+            }
+        }
+        System.out.println("target:" + target);
+    }
+}
+
+
+
 
 
 
